c606adadbfc333b78c9e8104534e154a4cbfde91
Joseph T Lyons <JosephTLyons@gmail.com>
2021-07-11 04:15:24 -0400

    Conditional checks to ensure we can show previous and current DirectoryContainers

--- src/PathContainer.py
+++ src/PathContainer.py
@@ -17,10 +17,15 @@ class PathContainer:
     def print(self) -> None:
         print(Panel(f"Path: {self.path}", title="TermiFind", expand=True))
 
-        current_directory_container: DirectoryContainer = self.path_directory_containers[-1]
-        current_directory_container.print()
+        if len(self.path_directory_containers) > 1:
+            previous_directory_container: DirectoryContainer = self.path_directory_containers[-2]
+            previous_directory_container.print()
 
-        next_directory_container: Optional[DirectoryContainer] = current_directory_container.get_next_directory_container()
+        if len(self.path_directory_containers) > 0:
+            current_directory_container: DirectoryContainer = self.path_directory_containers[-1]
+            current_directory_container.print()
 
-        if next_directory_container:
-            next_directory_container.print()
+            next_directory_container: Optional[DirectoryContainer] = current_directory_container.get_next_directory_container()
+
+            if next_directory_container:
+                next_directory_container.print()
